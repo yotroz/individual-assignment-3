386c01cb0949b064e107e30fe6f04f747cb52f7c
Octavio <s.octavio90@gmail.comgit config --global user.email s.octavio90@gmail.comgit congfig --global user.name s.octavio90@gmail.comgit config --global user.name s.octavio90@gmail.com>
2018-12-13 15:18:08 +0100

    done tests

--- small-shop/stock.py
+++ small-shop/stock.py
@@ -7,4 +7,35 @@ Created on Wed Dec 12 20:35:45 2018
 """
 
 #%%
+class Product:
+  def __init__(self, name, quality):
+      self.name = name
+      self.quality = quality
+            
+            
+def recalculate_quality(product):
+    if product.name == "potato":
+        product.quality = product.quality - 0.5
+    elif product.name == "cheese":
+        product.quality = product.quality - 2
+    else:
+        if product.quality < 5:
+            product.quality -= 3
+
+   
+potato = Product("potato", 10)    
+potato1 = Product("potato", 100)    
+potato2 = Product("potato", .5)    
+  
+
+cheese = Product("cheese", 10)
+cheese1 = Product("cheese", 100)    
+cheese2 = Product("cheese", .5)    
+
+
+pasta = Product("pasta", 4)
+pasta1 = Product("pasta1", 20)
+
+pasta2 = Product("pasta1", 5)
+
 

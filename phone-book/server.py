b67e63f31c81413b7d565ef050ac4b5834b8d04b
Octavio <s.octavio90@gmail.comgit config --global user.email s.octavio90@gmail.comgit congfig --global user.name s.octavio90@gmail.comgit config --global user.name s.octavio90@gmail.com>
2018-12-13 21:09:12 +0100

    updated methods post, delete, put. All logic function works

--- phone-book/server.py
+++ phone-book/server.py
@@ -16,22 +16,11 @@ phonebook = {"pepe": "53535353",
 def get_phonebook():
     return jsonify(phonebook)
 
-@server.route("/add-contact/<phone>/<name>")
-def add_contact(phone, name): 
-    
-    
-    if name not in phonebook: 
-        
-        phonebook.update({name:phone})
-        return jsonify("you have added " + name + " with the phone number: " + phone)
-    
-    else: 
-        return jsonify("you have already added " + name)
     
 @server.route("/add-contact/<phone>/<name>", methods=["POST"])
 def add_contact_post(phone, name): 
     
-    
+
     if name not in phonebook: 
         
         phonebook.update({name:phone})
@@ -45,31 +34,33 @@ def add_contact_post(phone, name):
 def get_phone_by_name(name): 
     
     if name in phonebook: 
-        return name + "'s  phone number is " + phonebook[name]
+        return jsonify(name + "'s  phone number is " + phonebook[name])
     
     else: 
-        return "there is no " + name + " in your phonebook"
+        return jsonify("there is no " + name + " in your phonebook")
 
 
-@server.route("/delete-contact/<name>")
+@server.route("/delete-contact/<name>", methods=["DELETE"])
 def delete_contact_by_name(name):   
     if name not in phonebook: 
         
-        return "there is no " + name + " in your phonebook"
+        return jsonify("there is no " + name + " in your phonebook")
     else:
         del phonebook[name]
-        return name + " has been deleted from your phonebook :( "         
+        
+        return jsonify(name + " has been deleted from your phonebook :( ")         
 
-               
-@server.route("/update-contact/<name>/<phone>")
+
+                  
+@server.route("/update-contact/<name>/<phone>", methods=["PUT"])
 def update_contact(name, phone): 
     
     if name not in phonebook: 
-        return "There is no one in you phonebook with that name"
+        return jsonify("There is no one in you phonebook with that name")
     
     else: 
         phonebook[name] = phone
-        return name + " has been updated to: " + phone
+        return jsonify(name + " has been updated to: " + phone)
     
         
     

bb615b20f7c2a148e5a31418ae23e53ad55e43ec
Octavio <s.octavio90@gmail.comgit config --global user.email s.octavio90@gmail.comgit congfig --global user.name s.octavio90@gmail.comgit config --global user.name s.octavio90@gmail.com>
2018-12-13 21:10:41 +0100

    all functions to call server work

--- phone-book/client.py
+++ phone-book/client.py
@@ -8,3 +8,36 @@ Created on Thu Dec 13 09:30:27 2018
 
 #%%
 
+
+import requests
+
+def get_phonebook_client(): 
+    request = requests.get('http://127.0.0.1:5000/phonebook')
+    
+    return request.json() 
+
+
+def add_contact_client_post(phone, name): 
+    request = requests.post('http://127.0.0.1:5000/add-contact/{}/{}'.format(phone, name))
+    
+    return request.json()
+
+
+def get_phone_by_name_client(name): 
+    request = requests.get('http://127.0.0.1:5000/get-phone/{}'.format(name))
+    
+    return request.json()
+
+
+def delete_contact_by_name_client(name): 
+    request = requests.delete('http://127.0.0.1:5000/delete-contact/{}'.format(name))
+    
+    return request.json()
+
+def update_contact_client(name, phone):
+    request = requests.put('http://127.0.0.1:5000/update-contact/{}/{}'.format(name, phone))
+    
+    return request.json()
+    
+
+    

7979ec074450ff7b685010083d79533f83a84acb
Octavio <s.octavio90@gmail.comgit config --global user.email s.octavio90@gmail.comgit congfig --global user.name s.octavio90@gmail.comgit config --global user.name s.octavio90@gmail.com>
2018-12-13 09:22:39 +0100

    done

--- github-api/github.py
+++ github-api/github.py
@@ -1,3 +1,36 @@
 #%%
 
+#from ./gitignore import apikey 
+import requests
 
+
+
+def get_all_repositories(user): 
+    
+    response = requests.get("https://api.github.com/users/" + user + "/repos")
+            
+    repos = response.json()
+    
+    
+    all_repos = []
+            
+    for repo in repos: 
+        
+        element = {
+                    "description": repo["description"],       
+                    "name": repo["name"],
+                    "stars": repo["stargazers_count"],     
+                    "language": repo["language"], 
+                    "url": repo["url"] 
+                }
+        
+        all_repos.append(element)
+        
+        
+        
+    return all_repos
+        
+    
+#%%
+    
+    
